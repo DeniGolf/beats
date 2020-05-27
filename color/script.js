eab7911073aa9c6b1459744be95b58fdc9bf456e
DeniGolf <58985244+DeniGolf@users.noreply.github.com>
2020-05-27 13:54:42 +0300

    Add files via upload

--- color/script.js
+++ color/script.js
@@ -353,7 +353,13 @@ function savePalette() {
       colors.push(hex.innerText);
     });
     //Generate Object
-    let paletteNr = savedPalettes.length;
+    let paletteNr;
+    const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+    if (paletteObjects) {
+      paletteNr = paletteObjects.length;
+    } else {
+      paletteNr = savedPalettes.length;
+    }
     const paletteObj = { name, colors, nr: paletteNr };
     savedPalettes.push(paletteObj);
     //Save to Local Storage
@@ -439,9 +445,10 @@ function saveToLocal(paletteObj) {
 
 function getLocal() {
   if (localStorage.getItem("palettes") === null) {
-    localStorage = [];
+    localPalettes = [];
   } else {
     const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+    savedPalettes = [...paletteObjects];
     paletteObjects.forEach((paletteObj) => {
       //Generate the palette for library
       const palette = document.createElement("div");
