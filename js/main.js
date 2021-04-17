920df7e61acd208580c253b791136dfc9d21b508
Denis <denis.klemeshov@gmail.com>
2021-04-17 13:52:32 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-let tag = document.createElement('script');
-let playBtn = document.querySelector("#playBtn");
-let pauseBtn = document.querySelector("#pauseBtn");
-let runOffBtn = document.querySelector("#runOffBtn");
-let runOffInput = document.querySelector("#runOffInput");
+const tag = document.createElement("script");
+const playBtn = document.querySelector("#playBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const runOffBtn = document.querySelector("#runOffBtn");
+const runOffInput = document.querySelector("#runOffInput");
 const phoneInput = document.querySelector("#phoneInput");
 const nameInput = document.querySelector("#nameInput");
 const statusMessage = document.querySelector("#statusMessage");
 
 // Youtube API
 tag.src = "https://www.youtube.com/iframe_api";
-let firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 let player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
- 
 
-    videoId: 'MPmyncxmsus',
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    videoId: "MPmyncxmsus",
     events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
   });
 }
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
 
-  playBtn.addEventListener("click", function() {
+function onPlayerReady(event) {
+  playBtn.addEventListener("click", function () {
     player.playVideo();
   });
-  
 
-  pauseBtn.addEventListener("click", function() {
+  pauseBtn.addEventListener("click", function () {
     player.pauseVideo();
   });
 
-  runOffBtn.addEventListener("click", function() {
+  runOffBtn.addEventListener("click", function () {
     let seconds = Number.isFinite(+runOffInput.value) ? +runOffInput.value : 0;
     player.pauseVideo();
     player.seekTo(seconds);
@@ -47,10 +41,7 @@ function onPlayerReady(event) {
   });
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-
-var done = false;
+let done = false;
 
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
@@ -58,106 +49,117 @@ function onPlayerStateChange(event) {
   }
 }
 
-
 //Phone number mask
-Array.prototype.forEach.call(document.body.querySelectorAll("*[data-mask]"), applyDataMask);
+function doFormat(x, pattern, mask) {
+  let strippedValue = x.replace(/[^0-9]/g, "");
+  let chars = strippedValue.split("");
+  let count = 0;
 
-function applyDataMask(field) {
-    var mask = field.dataset.mask.split('');
-    
-    // For now, this just strips everything that's not a number
-    function stripMask(maskedData) {
-        function isDigit(char) {
-            return /\d/.test(char);
-        }
-        return maskedData.split('').filter(isDigit);
+  let formatted = "";
+
+  for (let i = 0; i < pattern.length; i++) {
+    const c = pattern[i];
+    if (chars[count]) {
+      if (/\*/.test(c)) {
+        formatted += chars[count];
+        count++;
+      } else {
+        formatted += c;
+      }
+    } else if (mask) {
+      if (mask.split("")[i]) formatted += mask.split("")[i];
     }
-    
-    // Replace `_` characters with characters from `data`
-    function applyMask(data) {
-        return mask.map(function(char) {
-            if (char != '_') return char;
-            if (data.length == 0) return char;
-            return data.shift();
-        }).join('')
-    }
-    
-    function reapplyMask(data) {
-        return applyMask(stripMask(data));
-    }
-    
-    function changed() {   
-        var oldStart = field.selectionStart;
-        var oldEnd = field.selectionEnd;
-        
-        field.value = reapplyMask(field.value);
-        
-        field.selectionStart = oldStart;
-        field.selectionEnd = oldEnd;
-    }
-    
-    field.addEventListener('click', changed)
-    field.addEventListener('keyup', changed)
+  }
+  return formatted;
 }
 
-let map;
+phoneInput.addEventListener("focus", (e) => {
+  function format(elem) {
+    const val = doFormat(elem.value, elem.getAttribute("data-format"));
+    elem.value = doFormat(
+      elem.value,
+      elem.getAttribute("data-format"),
+      elem.getAttribute("data-mask")
+    );
+
+    if (elem.createTextRange) {
+      let range = elem.createTextRange();
+      range.move("character", val.length);
+      range.select();
+    } else if (elem.selectionStart) {
+      if (val.length !== 0) {
+        elem.setSelectionRange(val.length, val.length);
+      }
+    }
+  }
+  phoneInput.addEventListener("keyup", function () {
+    format(phoneInput);
+  });
+  format(phoneInput);
+});
 
 // Google Maps API
-function initMap() {
-  const center = {lat: 50.490846593594256, lng: 30.50709543848514};
 
-  const markerPos1 = {lat: 50.50957728551686, lng: 30.650867465579285};
-  const markerPos2 = {lat:  50.49856013105758, lng: 30.392770694048842};
-  const markerPos3 = {lat: 50.42688596643492, lng: 30.45108786166534};
+let map;
+function initMap() {
+  const center = { lat: 50.490846593594256, lng: 30.50709543848514 };
+
+  const markerPos1 = { lat: 50.50957728551686, lng: 30.650867465579285 };
+  const markerPos2 = { lat: 50.49856013105758, lng: 30.392770694048842 };
+  const markerPos3 = { lat: 50.42688596643492, lng: 30.45108786166534 };
 
   map = new google.maps.Map(document.getElementById("map"), {
     center,
-    // disableDefaultUI: true,
+    disableDefaultUI: true,
     zoom: 12,
   });
 
   const marker1 = new google.maps.Marker({
     position: markerPos1,
     map: map,
-    icon: "img/marker-green.svg"
+    icon: "img/marker-green.svg",
   });
 
   const marker2 = new google.maps.Marker({
     position: markerPos2,
     map: map,
-    icon: "img/marker-orange.svg"
+    icon: "img/marker-orange.svg",
   });
 
   const marker3 = new google.maps.Marker({
     position: markerPos3,
     map: map,
-    icon: "img/marker-orange.svg"
+    icon: "img/marker-orange.svg",
   });
 }
 
 // Google Sheets POST
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwJUCQs_Xj3vOPgdzcLRiDlJCjFk2sPUHo044LZ3J236v1IosCVqBnc2gfIEAdEt1M-/exec'
-  const form = document.forms['google-sheet']
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    const formInputValue = nameInput.value.trim();
-    let submitted = false;
-    let span = document.createElement("span");
-        span.classList.add("span-message");
-        span.innerHTML= "Form Submitting...";
-        statusMessage.appendChild(span);
-    
-    if(formInputValue.length > 3) {
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwJUCQs_Xj3vOPgdzcLRiDlJCjFk2sPUHo044LZ3J236v1IosCVqBnc2gfIEAdEt1M-/exec";
+const form = document.forms["google-sheet"];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formInputValue = nameInput.value.trim();
+  let submitted = false;
+  let span = document.createElement("span");
+  span.classList.add("span-message");
+  span.innerHTML = "Form Submitting...";
+  statusMessage.appendChild(span);
+
+  if (formInputValue.length > 3) {
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
         statusMessage.removeChild(span);
 
         span.classList.add("success-message");
         const link = document.createElement("a");
         link.setAttribute("target", "_blank");
-        link.setAttribute("href", "https://docs.google.com/spreadsheets/d/1lbf-QDdMA7Rcg0lvTuZp9xTlNxZzfoQEC9Jb_nJPiLM/edit?usp=sharing");
+        link.setAttribute(
+          "href",
+          "https://docs.google.com/spreadsheets/d/1lbf-QDdMA7Rcg0lvTuZp9xTlNxZzfoQEC9Jb_nJPiLM/edit?usp=sharing"
+        );
         link.innerHTML = "Google Sheets";
-        span.innerHTML= "Data has been sent to ";
+        span.innerHTML = "Data has been sent to ";
         span.appendChild(link);
         statusMessage.appendChild(span);
 
@@ -165,26 +167,18 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwJUCQs_Xj3vOPgdzcLRi
         phoneInput.value = "";
 
         setTimeout(() => statusMessage.removeChild(span), 8000);
-
       })
-      .catch(error => console.error('Error!', error.message))
-    } else {
-      statusMessage.removeChild(span);
+      .catch((error) => console.error("Error!", error.message));
+  } else {
+    statusMessage.removeChild(span);
 
+    span.classList.add("error-message");
+    span.innerHTML = "Your name must be at least <b>3</b> characters!";
+    statusMessage.appendChild(span);
 
-      span.classList.add("error-message");
-      span.innerHTML= "Your name must be at least <b>3</b> characters!";
-      statusMessage.appendChild(span);
+    nameInput.value = "";
+    phoneInput.value = "";
 
-      nameInput.value = "";
-      phoneInput.value = "";
-      
-      setTimeout(() => statusMessage.removeChild(span), 3000);
-    }
-  })
-
-
-
-
-
-
+    setTimeout(() => statusMessage.removeChild(span), 3000);
+  }
+});
